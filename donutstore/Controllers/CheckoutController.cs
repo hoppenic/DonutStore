bd0269850581605e2e7c79c106f757c2fe179560
hoppenic <hoppenic@gmail.com>
2018-06-16 12:04:21 -0500

    braintree

--- donutstore/Controllers/CheckoutController.cs
+++ donutstore/Controllers/CheckoutController.cs
@@ -3,14 +3,137 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Braintree;
+using donutstore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
 
 namespace donutstore.Controllers
 {
     public class CheckoutController : Controller
     {
-        public IActionResult Index()
+        private DonutStoreDbContext _donutStoreDbContext;
+        private EmailService _emailService;
+        private SignInManager<DonutStoreUser> _signInManager;
+        private BraintreeGateway _brainTreeGateway;
+
+        public CheckoutController(DonutStoreDbContext donutStoreDbContext, EmailService emailService, SignInManager<DonutStoreUser> signInManager,BraintreeGateway braintreeGateway)
         {
-            return View();
+            _donutStoreDbContext = donutStoreDbContext;
+            _emailService = emailService;
+            _signInManager = signInManager;
+            _brainTreeGateway = braintreeGateway;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            CheckoutViewModel model = new CheckoutViewModel();
+            await GetCurrentCart(model);
+            if (model.Cart == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+
+        private async Task GetCurrentCart(CheckoutViewModel model)
+        {
+            Guid cartId;
+            Cart cart = null;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var currentUser = await _signInManager.UserManager.GetUserAsync(User);
+                model.ContactEmail = currentUser.Email;
+                model.ContactPhoneNumber = currentUser.PhoneNumber;
+            }
+
+            if (Request.Cookies.ContainsKey("cartId"))
+            {
+                if (Guid.TryParse(Request.Cookies["cartId"], out cartId))
+                {
+                    cart = await _donutStoreDbContext.Carts
+                        .Include(carts => carts.CartItems)
+                        .ThenInclude(cartitems => cartitems.Product)
+                        .FirstOrDefaultAsync(x => x.CookieIdentifier == cartId);
+                }
+            }
+            model.Cart = cart;
+
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Index(CheckoutViewModel model)
+        {
+            await GetCurrentCart(model);
+
+            if (ModelState.IsValid)
+            {
+                Order newOrder = new Order
+                {
+                    TrackingNumber = Guid.NewGuid().ToString(),
+                    OrderDate = DateTime.Now,
+                    OrderItems = model.Cart.CartItems.Select(x => new OrderItem
+                    {
+                        ProductID = x.Product.ID,
+                        ProductName = x.Product.Name,
+                        ProductPrice = (x.Product.Price ?? 0),
+                        Quantity = x.Quantity
+                    }).ToArray(),
+                    AddressLine1 = model.ShippingAddressLine1,
+                    AddressLine2 = model.ShippingAddressLine2,
+                    Country = model.ShippingCountry,
+                    Email = model.ContactEmail,
+                    PhoneNumber = model.ContactPhoneNumber,
+                    Locale = model.ShippingLocale,
+                    PostalCode = model.ShippingPostalCode,
+                    Region = model.ShippingRegion
+
+                };
+
+                TransactionRequest transaction = new TransactionRequest
+                {
+                    //Amount = 1,
+                    Amount = model.Cart.CartItems.Sum(x => x.Quantity * (x.Product.Price ?? 0)),
+                    CreditCard = new TransactionCreditCardRequest
+                    {
+                        Number = model.BillingCardNumber,
+                        CardholderName = model.BillingNameOnCard,
+                        CVV = model.BillingCardVerificationValue,
+                        ExpirationMonth = model.BillingCardExpirationMonth.ToString().PadLeft(2, '0'),
+                        ExpirationYear = model.BillingCardExpirationYear.ToString()
+                    }
+
+                };
+                var transactionResult = await _brainTreeGateway.Transaction.SaleAsync(transaction);
+                if (transactionResult.IsSuccess())
+                {
+
+
+
+                    _donutStoreDbContext.Orders.Add(newOrder);
+                    _donutStoreDbContext.CartItems.RemoveRange(model.Cart.CartItems);
+                    _donutStoreDbContext.Carts.Remove(model.Cart);
+                    await _donutStoreDbContext.SaveChangesAsync();
+                    //Try to checkout
+                    Response.Cookies.Delete("cartId");
+                    return RedirectToAction("Index", "Receipt", new { id = newOrder.TrackingNumber });
+                }
+                for (int i = 0; i < transactionResult.Errors.Count; i++)
+                {
+                    ModelState.AddModelError("BillingCardNumber" + i, transactionResult.Errors.All()[i].Message);
+                }
+
+            }
+
+
+            return View(model);
+
+
         }
     }
-}
--- donutstore/Models/DonutStoreDbContext.cs
+++ donutstore/Models/DonutStoreDbContext.cs
@@ -29,6 +29,9 @@ namespace donutstore.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
     }
 
 
@@ -64,4 +67,40 @@ namespace donutstore.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
+
+    public class Order
+    {
+        public Order()
+        {
+            this.OrderItems = new HashSet<OrderItem>();
+        }
+
+        public int ID { get; set; }
+        public string TrackingNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public string ContactEmail { get; set; }
+        public string ContactPhoneNumber { get; set; }
+        public string ShippingAddress { get; set; }
+        public string ShippingLocale { get; set; }
+        public string ShippingRegion { get; set; }
+        public string ShippingCountry { get; set; }
+        public string ShippingPostalCode { get; set; }
+
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public int ID { get; set; }
+        public Order Order { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+
+    }
+
+
 }
